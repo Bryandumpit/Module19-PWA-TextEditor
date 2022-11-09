95f64978b704bd8f914b70b873fe1ea2f37c990d
Bryan Dumpit <dumpitbr@gmail.com>
2022-11-09 06:55:05 -0500

    added webpack configuration: htmlwebpack plugin, and css loaders and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,20 +10,41 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './src/js/index.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template:'./index.html',
+        title: 'Webpack Plugin'
+      }),
+      
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
